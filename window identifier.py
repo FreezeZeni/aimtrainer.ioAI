ce14e95758d474f89147b43806a6cb21108f146e
Zadumalsya <118173725+FreezeZeni@users.noreply.github.com>
2025-06-24 16:49:07 +0300

    Update window identifier.py

--- window identifier.py
+++ window identifier.py
@@ -1,27 +1,21 @@
 from PIL import Image
 import pyautogui
 
-# Получаем разрешение экрана
 screen_width, screen_height = pyautogui.size()
 
-# Заданные координаты и размеры области
 top = 362
 left = 551
 width = 801
 height = 481
 
-# Расчет новых координат с округлением
 new_left = round(left * screen_width / 1920)
 new_width = round(width * screen_width / 1920)
 new_top = round(top * screen_height / 1080)
 new_height = round(height * screen_height / 1080)
 
-# Создаем скриншот области экрана
 screenshot = pyautogui.screenshot(region=(left, top, width, height))
 
-# Сохраняем скриншот
 screenshot.save("screenshot.png")
 
-# Открываем и отображаем скриншот
 image = Image.open("screenshot.png")
 image.show()
